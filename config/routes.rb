64367d785b0e4db23089cae9c3e25277b37d8d80
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-10-29 22:03:14 -0400

    add scaffold for post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+	# get '/posts' => 'posts#index'
+	# get '/posts/:id' => 'posts#show'
+	# get '/posts/new' => 'posts#new'
+	# get '/posts/:id/edit' => 'posts#edit'
+	# post '/posts' => 'posts#create'
+	# patch '/posts/:id' => 'posts#update'
+	# delete '/posts/:id' => 'posts#destroy'
+	#This is what the resources:posts does, creating these routes
+  resources :posts
+  	# member do
+  	# 	get 'bobolinks' => 'posts#bobolinks'
+  	# end
+  	# collection do
+  	# 	get 'tweet' => 'posts#tweet'
+  	# end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
