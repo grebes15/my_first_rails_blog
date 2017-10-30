034320b55059ab9aa2449ee8814ebb9d6b2a15cc
Andreas Bach Landgrebe <andreaslandgrebe@gmail.com>
2017-10-30 11:00:06 -0400

    used scaffolding to create posts for my first blog

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
 	# delete '/posts/:id' => 'posts#destroy'
 	#This is what the resources:posts does, creating these routes
   resources :posts
+
+
+  root 'posts#index'
   	# member do
   	# 	get 'bobolinks' => 'posts#bobolinks'
   	# end
   	# collection do
   	# 	get 'tweet' => 'posts#tweet'
   	# end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
